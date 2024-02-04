5e5a95e2034a221bac96346bc01b9803b22a11ce
Tokair <miguellobo4841@gmail.com>
2024-02-04 14:59:23 -0300

    added double string mini project

--- creditcard.py
+++
@@ -1,4 +0,0 @@
-credit_card = (input("Insert a credit card number: "))
-credit_card2 = credit_card.replace(" " , "").replace("-" , "")
-first_digit = credit_card2[:4]
-print(f"Your credit card's first four characters are: {first_digit}")
---
+++ doublestring.py
@@ -0,0 +1,5 @@
+string = input("Insert a string to double every letter in it: ")
+output =""
+for i in string:
+    output +=  i*2
+print(output)
