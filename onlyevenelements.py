91ac8cc0c971661984607b4af04e9294ef6908cd
Tokair <miguellobo4841@gmail.com>
2024-03-16 08:19:25 -0300

    Esse novo código consegue suportar erros

--- onlyevenelements.py
+++ onlyevenelements.py
@@ -1,9 +1,12 @@
-#this code's goal is to show a list with only even numbers typed by the user.
+#o objetivo desse código é pegar todos os números pares digitados pelo usuário, somente os pares
 evenlist = []
 while True:
-    even = input('Insira um número, para terminar digite "terminar" : ')
-    if even == "terminar":
-        print("Todos os números pares dentre os números digitados: " , evenlist)
-        break
-    elif int(even) % 2 == 0:
-        evenlist.append(even)
+    try:
+        even = input('Insira um número, para terminar digite "terminar" : ')
+        if even == "terminar":
+            print("Todos os números pares dentre os números digitados: " , evenlist)
+            break
+        elif int(even) % 2 == 0:
+            evenlist.append(even)
+    except:
+        print("Digite um número válido")
